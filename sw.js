d6fa087557c312c58ca88e950e9af9201ed680c1
Sindicato Nacional dos Jogadores de Futebol da Guiné-Bissau <snjgb2025@gmail.com>
2026-08-15 15:08:46 +0000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = "snjgb-app-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/estilo.css",
+  "/script.js",
+  "/manifest.webmanifest"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
